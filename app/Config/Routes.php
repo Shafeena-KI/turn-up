043760e955f2497e9b77cbf52140a6b20f2c0efe
sandra <sandraachu2211@gmail.com>
2025-11-04 12:26:08 +0530

    routes event controller role controller role model

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,7 +6,17 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+//admin login
 $routes->post('api/admin/login', 'Api\Login::adminLogin');
+
+//user login
+$routes->post('api/user/login', 'Api\AppUser::UserLogin');
+
+//registration
+$routes->post('api/user/register', 'Api\AppUser::register');
+$routes->post('api/user/getuser', 'Api\AppUser::getUserById');
+$routes->post('api/user/update', 'Api\AppUser::updateUser');
 
 
 
@@ -14,5 +24,13 @@ $routes->post('api/admin/login', 'Api\Login::adminLogin');
 $routes->get('api/events', 'Api\EventController::index');
 $routes->get('api/events/(:num)', 'Api\EventController::show/$1');
 $routes->post('api/events/create', 'Api\EventController::create');
-$routes->post('api/events/(:num)', 'Api\EventController::update/$1');
-$routes->delete('api/events/(:num)', 'Api\EventController::delete/$1');
+$routes->post('api/events/update', 'Api\EventController::update');
+$routes->delete('api/events/delete', 'Api\EventController::delete');
+
+
+//Roles and Permissions 
+$routes->get('api/roles', 'Api\RoleController::index');
+$routes->get('api/roles/(:num)', 'Api\RoleController::show/$1');
+$routes->post('api/roles/create', 'Api\RoleController::create');
+$routes->post('api/roles/update', 'Api\RoleController::update');
+$routes->delete('api/roles/delete', 'Api\RoleController::delete');
