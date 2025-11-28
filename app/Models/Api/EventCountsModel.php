0c6cd05be6081c2e18025947a287e8dd7def1163
sandra <sandraachu2211@gmail.com>
2025-11-28 08:27:51 +0530

    checkin event booking and invite

--- app/Models/Api/EventCountsModel.php
+++ app/Models/Api/EventCountsModel.php
@@ -12,7 +12,11 @@ class EventCountsModel extends Model
     protected $allowedFields = [
         'event_id', 'category_id',
         'total_invites', 'total_male_invites', 'total_female_invites', 'total_couple_invites',
+        'total_other_invites',
         'total_booking', 'total_male_booking', 'total_female_booking', 'total_couple_booking',
-        'total_checkin', 'total_male_checkin', 'total_female_checkin', 'total_couple_checkin'
+        'total_other_booking',
+        'total_checkin', 'total_male_checkin', 'total_female_checkin', 'total_couple_checkin',
+        'total_other_checkin',
+        'updated_at'
     ];
 }
